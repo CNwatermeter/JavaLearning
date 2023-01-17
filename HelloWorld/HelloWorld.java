cf6c221b11b47fddb2d48126540d453800ec2c50
CNwatermeter <1198816720@qq.com>
2023-01-17 13:04:22 +0800

    Hello World

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
+    //the first java program
     public static void main(String[] args) {
         //the entrance of Java program
         System.out.println("Hello world!");
