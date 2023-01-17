12889c67db3be1cdf9e5bf81566ccc1136c30668
CNwatermeter <1198816720@qq.com>
2023-01-17 16:52:33 +0800

    variable learning about int&double

---
+++ variable/variable.java
@@ -0,0 +1,11 @@
+public class variable{
+    public static void main(String[] args){
+        //int: stores a integer
+        int integer1 = 100;
+        //double: stores a floating-point number
+        double floatingpointnumber1 = 3.14;
+        //printing test
+        System.out.println(integer1);
+        System.out.println(floatingpointnumber1);
+    }
+}
