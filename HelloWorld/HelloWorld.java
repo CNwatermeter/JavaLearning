2dcbe962c32d36d06081c91747d14e560cb4959c
CNwatermeter <1198816720@qq.com>
2023-01-17 12:59:51 +0800

    some literal demo in java

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        //the entrance of Java program
         System.out.println("Hello world!");
+        //print character in screen
     }
 }
---
+++ literal/literal.java
@@ -0,0 +1,21 @@
+public class literal {
+    public static void main(String[] args) {
+        //integers
+        System.out.println(100);
+        System.out.println(-100);
+        //floating-point numbers
+        System.out.println(3.14);
+        //characters
+        System.out.println('@');
+        //special characters
+        //tab character: used to create a tab space between text
+        System.out.println("name"+'\t'+"age");
+        System.out.println("tom"+'\t'+"24");
+        //strings
+        System.out.println("爪哇");
+        //boolean values only includes true and false two values
+        System.out.println(true);
+        System.out.println(false);
+        //and null values, it can not be printed
+    }
+}
