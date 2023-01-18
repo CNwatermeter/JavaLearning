093c592526d6e2036d7dd3d767cdab8191d39ad3
CNwatermeter <1198816720@qq.com>
2023-01-18 20:54:06 +0800

    learning how to manage basic program through IDEA

---
+++ IdeaProject/TheFirstModule/src/com/wws0629/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+/**Project: File settings
+ * Module: In project structure settings when a project is exiting
+ * Package: Right-click a file and new a package
+ * Class: Use keyword 'public class'
+ *        If you need to modify name of class, right-click class want to modify
+ *        and click 'Refactor', the first option is 'Rename'*/
+package com.wws0629.demo;
+//the name of package usually is 'com' + company's name + function
+//both part should add a point in there
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
