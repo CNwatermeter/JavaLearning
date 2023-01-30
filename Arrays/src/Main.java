3a54c12c824cd8c64eead964b7f2ecaa3d17c321
CNwatermeter <1198816720@qq.com>
2023-01-30 15:06:42 +0800

    usage of an array

---
+++ Arrays/src/Main.java
@@ -0,0 +1,17 @@
+/**Array
+ * A array is a data structure in programming that is used to store a collection of
+ * elements, all of them is same data type. Each element in the array is stored at
+ * a specific index or location, and can be accessed using that index.*/
+public class Main {
+    public static void main(String[] args) {
+        //to define an array, we can use two form:
+        //int[] array
+        //int array[]
+        //but we often use the first form
+        int[] array1 = new int[]{1, 2, 3}; //static initialization, complete form
+        int[] array2 = {1, 2, 3}; //handy form
+        //once an array is defined, the content also is fixed
+        //so, both array1 and array2 are 3-long array
+        
+    }
+}
