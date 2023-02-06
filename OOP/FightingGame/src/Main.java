2ba564f0f117ef1fcb07c7d5ef78d9aac847271a
CNwatermeter <1198816720@qq.com>
2023-02-06 15:45:51 +0800

    object-oriented programming

--- OOP/FightingGame/src/Main.java
+++ OOP/FightingGame/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Role r1 = new Role("Alpha", 100); //set role
+        Role r2 = new Role("Beta", 100);
+        while(true){
+            r1.attack(r2);
+            if (r2.getBlood() > 0) {
+                //System.out.println(r2.getName() + " HP=" + r2.getBlood());
+                System.out.printf("%s HP=%s\n", r2.getName(), r2.getBlood());
+                //'%s' is a placeholder
+                //'\n' is a line break
+            } else {
+                //System.out.println(r2.getName() + " is dead");
+                System.out.printf("%s is dead", r2.getName());
+                break;
+            }
+            r2.attack(r1);
+            if (r1.getBlood() > 0) {
+                System.out.printf("%s HP=%s\n", r1.getName(), r1.getBlood());
+            }else{
+                System.out.printf("%s is dead", r1.getName());
+                break;
+            }
+        }
     }
 }
--- OOP/FightingGame/src/Role.java
+++ OOP/FightingGame/src/Role.java
@@ -1,6 +1,9 @@
+import java.util.Random;
+
 public class Role {
     private String name;
     private int blood;
+
     public String getName() {
         return name;
     }
@@ -17,9 +20,26 @@ public class Role {
         this.blood = blood;
     }
 
+    //function method//
+    public void attack(Role role) {
+        System.out.printf("%s attacked %s\n", this.getName(), role.getName());
+        //all variable that don't use 'this' is the role variable
+        //we can use this method receive all role's variable and method
+        //role and this both are address
+        Random r = new Random();
+        int injury = r.nextInt(10) + 10;
+        System.out.printf("%s -%sHP\n", role.getName(), injury);
+        //give a random attack injury
+        int remainBlood = role.getBlood() - injury;
+        //remainBlood = remainBlood < 0 ? 0 : remainBlood;
+        role.setBlood(remainBlood);
+    }
+
+
     //constructor//
     public Role() {
     }
+
     public Role(String name, int blood) {
         this.name = name;
         this.blood = blood;
